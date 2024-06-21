477caaaec255c6c48d86e53247f3a1bb9e38d77f
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2024-06-21 23:28:52 +0530

    Added react element

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
-const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Hello world from REACT!")
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const TitleHeading =()=> (
+<h1 id="heading">
+namste avinav
+</h1>);
+
+
+const HeadingComponent = ()=>{
+    return <div>
+      <TitleHeading />
+      <h1>this is avi</h1>
+      </div>;
+}
+ 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(<HeadingComponent />);
